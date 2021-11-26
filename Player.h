58ec6dbe4be837976930657f1c89d2cabc13297a
trgvy23 <81300455+trgvy23@users.noreply.github.com>
2021-11-26 21:05:54 +0700

    Flip obstacles and implement class Player

---
+++ Player.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "Position.h"
+#include "Enemy.h"
+#include <string>
+#include <vector>
+#include <conio.h>
+
+class PLAYER {
+private:
+	POSITION pos;
+	bool isDead;
+	bool isWin;
+	const int width = 1;
+	const int height = 1;
+public:
+	PLAYER(POSITION position, bool dead = false, bool win = false);
+	void moveLeft();
+	void moveRight();
+	void moveUp();
+	void moveDown();
+	void drawPlayer();
+	void deleteOldPlayer();
+	bool isCollide(int posX, int posY, Enemy* currentEnemy);
+	bool checkCrash(vector<Enemy*> enemy);
+	void crashEffect();
+	void drawEffect(char** shape, int width, int height);
+};
