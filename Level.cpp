66d4abadb0f5368224d7e4ab3167bc16f1bb8975
trgvy23 <81300455+trgvy23@users.noreply.github.com>
2021-11-30 16:22:53 +0700

    Fix file text and implement Level

---
+++ Level.cpp
@@ -0,0 +1,14 @@
+﻿#include "Level.h"
+
+LEVEL::LEVEL(int nLevel, int speed, int distance) {
+	this->nLevel = nLevel;
+	this->speed = speed;
+	this->distance = distance;
+}
+
+//set tạm sau này điều chỉnh
+void LEVEL::NewLevel() {
+	nLevel++;
+	speed -= 10;
+	distance--;
+}
---
+++ Level.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include <iostream>
+#include "Enemy.h"
+#include "Player.h"
+#include "Position.h"
+#include "Map.h"
+#include "SingleRow.h"
+
+using namespace std;
+
+class LEVEL {
+private:
+	int nLevel;
+	int speed;
+	int distance;
+	//int nEnemy; //lên level thì tăng số lượng enemy? để đây phòng hờ mn ok thì t thêm zô
+public:
+	LEVEL(int nLevel = 1, int speed = 200, int distance = 7);
+	void NewLevel();
+
+};
