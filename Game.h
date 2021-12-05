7a6a2eda88f94c4c106d8cbaa7e37b342bfe88d9
trgvy23 <81300455+trgvy23@users.noreply.github.com>
2021-12-05 18:35:03 +0700

    menu

---
+++ Game.h
@@ -0,0 +1,13 @@
+#ifndef GAME_H_
+#define GAME_H_
+#include <iostream>
+#include<conio.h>
+#include "ConsoleManagement.h"
+using namespace std;
+
+class GAME {
+public:
+	void menu();
+};
+
+#endif // !GAME_H_
