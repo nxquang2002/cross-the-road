2f121fe4f5481eb924a527afdc130d28eeb02134
nxquang2002 <nguyenxquang01102002@gmail.com>
2021-12-05 22:10:47 +0700

    Merger Player and Enemy async movement

--- Map.cpp
+++ Map.cpp
@@ -1,6 +1,7 @@
 #include "Map.h"
 
-MAP::MAP() : width(120), height(35), isPause(0), isEnd(0) {
+
+MAP::MAP() : width(120), height(35), isPause(0), isEnd(0), player(POSITION(50, 33)), level(LEVEL(1)) {
 	for (int i = 0; i <= width; ++i) {
 		map[0][i] = map[height][i] = '-';
 	}
@@ -8,17 +9,14 @@ MAP::MAP() : width(120), height(35), isPause(0), isEnd(0) {
 	for (int i = 1; i < height; ++i) {
 		map[i][0] = map[i][width] = '|';
 		if (i % 5 == 0) {
-			for (int j = 1; j < width; ++j)
+			for (int j = 1; j < width; ++j) {
 				map[i][j] = '-';
+			}
 		}
 		else {
 			for (int j = 1; j < width; ++j)
 				map[i][j] = ' ';
 		}
-		if (i % 5 == 2 || i % 5 == 3) {
-			map[i][width + 1] = 254;
-		}
-		else map[i][width + 1] = ' ';
 	}
 }
 
@@ -35,6 +33,18 @@ void MAP::drawMap() {
 	}
 
 	drawRecSingle(130, 1, 40, 20);
+	gotoXY(145, 2);
+	cout << "INSTRUCTION";
+	gotoXY(135, 5);
+	cout << "[" << (char)30 << "]: Up";
+	gotoXY(135, 7);
+	cout << "[" << (char)31 << "]: Down";
+	gotoXY(135, 9);
+	cout << "[" << (char)16 << "]: Right";
+	gotoXY(135, 11);
+	wcout << "[" << (char)17 << "]: Left";
+	gotoXY(135, 13);
+	cout << "[P]: Pause";
 }
 
 void MAP::drawRecSingle(int ox, int oy, short width, short height) {
@@ -95,41 +105,83 @@ void MAP::drawObject(POSITION pos, char** shape, int width, int height) {
 	}
 }
 
+void MAP::drawPlayer() {
+	player.drawPlayer();
+}
 
-void MAP::newState() {
-	int ranRow = rand() % 5;
-	int ranEnemy = rand() % 4;
-	for (int i = 0; i < 5; ++i) {
-		if (i == ranRow) {
-			 
-		}
+void MAP::updatePlayerPos(char key) {
+	switch (key)
+	{
+	case 72:	//Up
+		player.moveUp();
+		break;
+	case 80:	//Down
+		player.moveDown();
+		break;
+	case 77:	//Right
+		player.moveRight();
+		break;
+	case 75:	//Left
+		player.moveLeft();
+		break;
+	default:
+		break;
 	}
 }
 
-// int main(){
 
-// 	resizeConsole(1300, 700);
-// 	MAP map;
-// 	map.drawMap();
+void MAP::newState() {
+	char key;
+	int speed = level.getSpeed();
+	int distance = level.getDistance();
+	int lightPhase = level.getLightPhase();
+	rows.initializeState(speed);
+	drawMap();
+	drawPlayer();
 
-// 	int count = 0;
-// 	while (count < 1) {
-// 		count++;
-// 		ENEMY* e = new Dinosaur(POSITION(5, 8));
-// 		e->getShape();
-// 		e->drawShape();
-// 		while (!e->isOutOfMap()) {
-// 			Sleep(200);
-// 			e->deleteOldEnemy();
-// 			e->move();
-// 			e->drawShape();
-// 		}
-// 		e->deleteOldEnemy();
-// 		e->~ENEMY();
-// 	}
+	int t = 0;
+	while (!isEnd) {
+		rows.newState(t, speed, 50000); //time - speed - lightPhase
+		if (_kbhit()) {
+			key = _getch();
+			if (key == 27) {
+				break;
+			}
+			else updatePlayerPos(key);
+		}
+		if (checkCrash()) {
+			player.crashEffect();
+			break;
+		}
+		t++;
+		if (t >= INT_MAX)  //To prevent t from overflow
+			t = 0;
+	}
+}
+
+bool MAP::checkCrash() {
+	vector<int> currentRows;
+	player.getCurrentRows(currentRows);
+	if (currentRows.empty())
+		return false;
+	vector<ENEMY*> listEnemies;
+	for (int i = 0; i < currentRows.size(); ++i) {
+		vector<ENEMY*> temp = rows.listEnemies(currentRows[i]);
+		for (int j = 0; j < temp.size(); ++j)
+			listEnemies.push_back(temp[j]);
+	}
+	if (player.checkCrash(listEnemies))
+		return true;
+	return false;
+}
 
 
-// 	system("pause");
-// 	return 0;
-// }
+int main(){
+	resizeConsole(1300, 700);
+	Nocursortype();
+ 	MAP map;
+	map.newState();
 
+ 	system("pause");
+	return 0;
+ }
--- Map.h
+++ Map.h
@@ -1,12 +1,16 @@
 #ifndef _MAP_H_
 #define _MAP_H_
 
-
 #include<iostream>
-#include "ConsoleManagement.h"
+#include "ConsoleManagement.h" 
 #include"POSITION.h"
 #include"Player.h"
 #include"Enemy.h"
+#include "Rows.h"
+#include "Level.h"
+#include <thread>
+#include<conio.h>
+#define INT_MAX 1000000		//To prevent overflow
 
 using namespace std;
 
@@ -17,6 +21,8 @@ private:
 	bool isPause;
 	bool isEnd;
 	PLAYER player;
+	ROWS rows;
+	LEVEL level;
 public:
 	MAP();
 	~MAP();
@@ -25,8 +31,11 @@ public:
 	void drawRecSingle(int ox, int oy, short width, short height);
 	void drawRecDouble(int ox, int oy, short width, short height);
 	void drawObject(POSITION pos, char** shape, int width, int height);
+	void drawPlayer();
 	void updatePlayerPos(char key);
 	void newState();
+	bool checkCrash();
 };
 
 #endif // !_MAP_H_
+
